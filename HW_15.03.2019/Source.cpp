62d92d9f33e53293273cb30e46ab7d1c8c259cb7
dinara0708 <aikenova01@gmail.com>
2019-03-17 04:16:06 +0600

    -

--- HW_15.03.2019/Source.cpp
+++ HW_15.03.2019/Source.cpp
@@ -7,31 +7,74 @@
 
 using namespace std;
 
+void fill_matrix(int **pp, int n, int m) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			pp[i][j] = rand() % 21;
+}
+void print_matrix(int**pp, int n, int m) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << setw(3) << left << pp[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void delete_matrix(int**pp, int n, int m) {
+	for (int i = 0; i < n; i++)
+		delete[] pp[i];
+	delete[] pp;
+}
+void erase_line(int **&pp, int &n, int m, int pos) {
+	int **tmp = new int*[n - 1];
+
+	for (int i = 0; i < pos; i++)
+		tmp[i] = pp[i];
+
+	for (int i = pos + 1; i < n + 1; i++)
+		tmp[i - 1] = pp[i];
+
+	delete[] pp;
+	pp = tmp;
+	n--;
+}
 // 1.	Написать программу, которая удаляет из двухмерного массива,
 // заполненного случайными числами строки, содержащие нулевые элементы.
-
-
+void eraseNullRows(int **&pp, int &n, int m) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (pp[i][j] == 0)
+			{
+				erase_line(pp, n, m, i);
+				break;
+			}
+		}
+	}
+}
 
 // 2.	Написать программу, которая поэлементно суммирует два динамических двумерных массива,
 // заполненных случайными числами и сохраняет результат в третьем массиве.
 // Размеры исходных массивов  задаются пользователем.
+int** sum_matrix(int**a, int**b, int n, int m) {
+	int **c = new int*[n];
+	for (int i = 0; i < n; i++)
+		c[i] = new int[m];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			c[i][j] = a[i][j] + b[i][j];
+
+	return c;
+	delete_matrix(c, n, m); // можно и после return чистить память?
+}
 
 
-void createArray(int *&a, int n) {
-	a = new int[n];
-}
-void fillArray(int *a, int n) {
-	for (int i = 0; i < n; i++)
-		a[i] = rand() % 50 - 10;
-}
-void printArray(const int *a, int n) {
-	for (int i = 0; i < n; i++)
-		cout << setw(3) << a[i] << " ";
-	cout << endl;
-}
-void deleteArray(int *&a) {
-	delete[] a;
-}
+
 
 
 int main()
@@ -51,52 +94,42 @@ int main()
 		{
 		case 1:
 		{
-
+			int **pp, n, m;
+			cin >> n >> m;
+			pp = new int*[n];
+			for (int i = 0; i < n; i++)
+				pp[i] = new int[m];
+			fill_matrix(pp, n, m);
+			print_matrix(pp, n, m);
+			eraseNullRows(pp, n, m);
+			print_matrix(pp, n, m);
+			delete_matrix(pp, n, m);
 		}
 		break;
 		case 2:
 		{
+			int **a, **b, **AB;
+			int n, m;
+			cin >> n >> m;
+			a = new int*[n];
+			b = new int*[n];
 
-		}
-		break;
-		case 3:
-		{
+			for (int i = 0; i < n; i++)
+			{
+				a[i] = new int[m];
+				b[i] = new int[m];
+			}
+			fill_matrix(a, n, m);
+			fill_matrix(b, n, m);
+			print_matrix(a, n, m);
+			print_matrix(b, n, m);
 
-		}
-		break;
-		case 4:
-		{
+			AB=sum_matrix(a, b, n, m);
+			print_matrix(AB, n, m);
 
-		}
-		break;
-		case 5:
-		{
-
-		}
-		break;
-		case 6:
-		{
-
-		}
-		break;
-		case 7:
-		{
-
-		}
-		break;
-		case 8:
-		{
-
-		}
-		break;
-		case 9:
-		{
-
-		}
-		break;
-		case 10:
-		{
-
+			delete_matrix(a, n, m);
+			delete_matrix(b, n, m);
+			// delete_matrix(AB, n, m);
 		}
 		break;
 		default:
